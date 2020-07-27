ac2e0ef1495c1df2207c2ee7e7aaa83e63a68768
Jordan Maguire <a.jordan.maguire@gmail.com>
2020-07-27 15:33:32 +0800

    Remove unnecessary subfolders

--- lib/iex/style.rb
+++
@@ -1,7 +0,0 @@
-require "iex/style/version"
-
-module IEX
-  module Style
-    # Nothing to see here.
-  end
-end
--- lib/iex/style/version.rb
+++
@@ -1,5 +0,0 @@
-module IEX
-  module Style
-    VERSION = "1.0.0".freeze
-  end
-end
