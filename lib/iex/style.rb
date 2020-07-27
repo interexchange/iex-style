f548fab7563e8ea6bf742e615b103d1298519f4c
Jordan Maguire <a.jordan.maguire@gmail.com>
2020-07-27 15:05:58 +0800

    Created a shared Rubocop configuration

---
+++ lib/iex/style.rb
@@ -0,0 +1,7 @@
+require "iex/style/version"
+
+module IEX
+  module Style
+    # Nothing to see here.
+  end
+end
---
+++ lib/iex/style/version.rb
@@ -0,0 +1,5 @@
+module IEX
+  module Style
+    VERSION = "1.0.0".freeze
+  end
+end
